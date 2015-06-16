db04b820e57ee785c7b80c18e804ff673e8ada3a
Andrey <andreykulikov95@gmail.com>
2015-06-16 22:10:28 +0300

    Initial commit. RESTful via Spring.

---
+++ src/main/java/ru/riskgap/integration/RequestParser.java
@@ -0,0 +1,10 @@
+package ru.riskgap.integration;
+
+/**
+ * Created by Nikita on 16.06.2015.
+ */
+public class RequestParser {
+    public static Task parse() {
+        return null;
+    }
+}
---
+++ src/main/java/ru/riskgap/integration/SmokeEntity.java
@@ -0,0 +1,39 @@
+package ru.riskgap.integration;
+
+import java.util.Date;
+
+public class SmokeEntity {
+    private int id;
+    private String name;
+    private Date date;
+
+    public SmokeEntity(int id, String name, Date date) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
