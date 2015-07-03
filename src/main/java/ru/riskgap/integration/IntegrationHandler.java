8cffd3266a890eff9859743870dfba26b95aa694
alexandraefanova <efanova.aleksandra@gmail.com>
2015-07-03 13:12:17 +0300

    add interface that specify tfs msproject and trello handler

---
+++ src/main/java/ru/riskgap/integration/IntegrationHandler.java
@@ -0,0 +1,22 @@
+package ru.riskgap.integration;
+
+
+/**
+ * Interface for three classes (TFS, Trello, Ms Project) that update or create Task On Server or get info from Server
+ */
+public interface IntegrationHandler {
+
+    /**
+     *
+     * @param task information from request that was parsed.
+     * @return changed task
+     */
+    public Task createOrUpdateTask(Task task);
+
+    /**
+     *
+     * @param task information about id, place of task, which info we want to get
+     * @return info about Task
+     */
+    public Task getTaskInformation(Task task);
+}
